bf93ee498120aa508265f27c56e6169c6afa5817
Natália S. Gomes <nataliandsg32@gmail.com>
2024-04-14 18:08:08 -0300

    Adicionada validação de entrada para o CEP na classe Consulta

--- src/buscador/Consulta.java
+++ src/buscador/Consulta.java
@@ -9,7 +9,11 @@ import java.net.http.HttpResponse;
 
 public class Consulta {
 
-    public Endereco buscaEndereco(String cep){
+    public Endereco buscaEndereco(String cep) {
+        if (!validarCep(cep)) {
+            throw new IllegalArgumentException("CEP inválido. O CEP deve conter apenas números e ter 8 dígitos.");
+        }
+
         URI end = URI.create("https://viacep.com.br/ws/" + cep + "/json/");
         HttpRequest request = HttpRequest.newBuilder().uri(end).build();
         HttpResponse<String> response = null;
@@ -17,8 +21,21 @@ public class Consulta {
         try {
             response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
             return new Gson().fromJson(response.body(), Endereco.class);
-        } catch (Exception e){
+        } catch (Exception e) {
             throw new RuntimeException("Não foi possível encontrar o endereço.");
         }
     }
+
+    private boolean validarCep(String cep) {
+        if (cep.length() != 8) {
+            return false;
+        }
+        for (char c : cep.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
+
