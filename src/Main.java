af4e78f80f37b2c0970220d746f2843d11481a4c
Natália S. Gomes <nataliandsg32@gmail.com>
2024-04-15 19:19:50 -0300

    Adiciona formatação de saída

--- src/Main.java
+++ src/Main.java
@@ -9,20 +9,36 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Digite um número de CEP para buscar: ");
-        String cep = scanner.nextLine();
+        System.out.print("Digite o(s) número(s) de CEP para buscar (separe múltiplos CEPs por vírgula): ");
+        String input = scanner.nextLine();
+
+        String[] ceps = input.split("[,\\s]+");
+
         Consulta consulta = new Consulta();
 
-        try {
-            Endereco endereco = consulta.buscaEndereco(cep);
-            System.out.println(endereco);
+        for (String cep : ceps) {
+            try {
+                cep = cep.trim();
+                Endereco endereco = consulta.buscaEndereco(cep);
 
-            GeradorDeArquivo geradorDeArquivo = new GeradorDeArquivo();
-            geradorDeArquivo.salvarJson(endereco);
+                System.out.println("------------------------");
+                System.out.printf("%-12s: %s\n", "CEP", endereco.cep());
+                System.out.printf("%-12s: %s\n", "Logradouro", endereco.logradouro());
+                System.out.printf("%-12s: %s\n", "Complemento", endereco.complemento());
+                System.out.printf("%-12s: %s\n", "Bairro", endereco.bairro());
+                System.out.printf("%-12s: %s\n", "Localidade", endereco.localidade());
+                System.out.printf("%-12s: %s\n", "UF", endereco.uf());
+                System.out.println("------------------------");
 
-        } catch (RuntimeException | IOException e) {
-            System.out.println(e.getMessage());
+                GeradorDeArquivo geradorDeArquivo = new GeradorDeArquivo();
+                geradorDeArquivo.salvarJson(endereco);
+
+            } catch (IllegalArgumentException e) {
+                System.out.println("Erro: " + e.getMessage() + " Ignorando o CEP: " + cep);
+            } catch (RuntimeException | IOException e) {
+                System.out.println("Erro ao buscar o endereço para o CEP " + cep + ": " + e.getMessage());
+            }
+
         }
-
     }
 }
